ab12c3f56c66cbb490dc1b78ece3c013cfb7b1e2
julianwe17 <julian.in.hbr@gmail.com>
2024-10-20 20:24:59 +0200

    Removed main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-import clashai
-client = clashai.Client(api_key="sk-r0A00DzpxGRLddtomPauVYs3RCOjzvk86nuDY4jgIpnuK1uX")
-response = client.chat.completions.create(messages=[{"role": "user", "content": "Hallo du bist cool"}], model="gpt-4-turbo")
-print(response)
